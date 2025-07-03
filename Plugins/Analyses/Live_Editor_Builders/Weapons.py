beb309746af760100bb6fa44d85c6fb072ac67e2
ahmetsad <53543484+ahmetsad@users.noreply.github.com>
2025-07-03 23:33:55 +0300

    Update Weapons.py

--- Plugins/Analyses/Live_Editor_Builders/Weapons.py
+++ Plugins/Analyses/Live_Editor_Builders/Weapons.py
@@ -180,8 +180,14 @@ bullet_item_macros = [
 
     E('damage'                    , './/damage'          , 'value'        , 'Damage', ''),
     E('damage_shield'             , './/damage'          , 'shield'       , '+Shield', ''),
-    E('damage_hull'               , './/damage'          , 'hull'         , '+Hull', ''),
+    E('damage_hull'               , './/damage'          , 'hull'         , 'phase', ''),
+    E('damage_hull'               , './/damage'          , 'noshield'         , '+hull', ''),
     E('damage_repair'             , './/damage'          , 'repair'       , 'Repair', 'Set to 1 to flip to repairing.'),
+    
+    E('areadamage'                    , './/areadamage'          , 'value'        , 'Damage', ''),
+    E('areadamage_shield'             , './/areadamage'          , 'shield'       , '+Shield', ''),
+    E('areadamage_hull'               , './/areadamage'          , 'noshield'         , '+Hull', ''),
+    E('areadamage_range'               , './/areadamage'          , 'range'         , 'radius', ''),
 
     E('bullet_speed'              , './/bullet'          , 'speed'        , 'Bullet Speed', ''),
     E('bullet_lifetime'           , './/bullet'          , 'lifetime'     , 'Bullet Lifetime', ''),
